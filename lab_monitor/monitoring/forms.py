8a38232da9f5778b70e1c70270dbed785aac8efa
Hemanth0411 <151835955+Hemanth0411@users.noreply.github.com>
2025-02-06 18:39:13 +0530

    Delete lab_monitor directory

--- lab_monitor/monitoring/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-from .models import Subject
-
-class SubjectSelectionForm(forms.Form):
-    subject = forms.ModelChoiceField(queryset=Subject.objects.all(), empty_label=None)
